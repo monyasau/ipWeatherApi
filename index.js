13aacaf042e936622735e3fe34c1f3bccac630c0
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-02 05:56:37 +0100

    Added temporary fix for local testing

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const port = 8081;
 
 app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || 'Guest'; // Default to 'Guest' if visitor_name is not provided
-    const clientIp = req.ip;
-    console.log(clientIp);
+    let clientIp = req.ip;
+    //remove conditional block in production
+    if(req.ip=="127.0.0.1"||"::1"){
+    clientIp="102.89.22.143"
+    console.log(clientIp)
+    }
 })
 app.listen(port, ()=>{
     console.log("running on pprt",port);
