b73d5ab07bdb495211692915f64ab7ca6b2c2c39
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-02 07:07:36 +0100

    Handle redundant or empty region in greeting message
    - Update greeting message construction to exclude region if it is the same as the city or empty.
    - Ensure location in the greeting is clear and concise without redundancy.
    - Improve error handling

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ app.get("/api/hello", async (req, res) => {
   let ipData = await axios.get("https://ipapi.co/json/");
   let clientIp = ipData.data.ip;
   let clientCity = ipData.data.city;
-  let clientRegion = ipData.data.region==clientCity?"":ipData.data.region;
+  let clientRegion = ipData.data.region;
   let weatherData = await axios.get(`http://api.weatherapi.com/v1/current.json?key=49e72d8fff664667a7485456231008&q=${clientCity}&aqi=no`)
   let temperature = weatherData.data.current.temp_c;
+  let greetingMessage =  `Hello ${visitorName}, the temperature is ${temperature} degrees Celcius in ${clientCity}`
+  if (clientRegion && clientRegion !== clientCity) {
+    greetingMessage += `, ${clientRegion}`
+  }
+
 
   let responseObject ={
     client_ip: clientIp,
     location: clientCity,
-    greeting: `Hello ${visitorName}, the temperature is ${temperature} degrees Celcius in ${clientCity}, ${clientRegion}`
+    greeting: greetingMessage,
   }
   res.json(responseObject)
 } catch (error) {
