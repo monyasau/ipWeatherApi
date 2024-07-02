50131a708d9b5ccc4ff2b5d7f98dbcdc1822c5d7
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-02 07:13:58 +0100

    updated listening port to 80 for accessibility

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = 8081;
+const port = 80;
 
 app.get("/api/hello", async (req, res) => {
   try{const visitorName = req.query.visitor_name || "Guest"; // Default to 'Guest' if visitor_name is not provided
