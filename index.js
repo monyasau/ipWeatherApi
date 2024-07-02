2427a8a8a355bfda1a4e8bf852c6bd88a823c95f
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-02 06:07:49 +0100

    Removed conditional statement

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 8081;
 
-app.get('/api/hello', async (req, res) => {
-    const visitorName = req.query.visitor_name || 'Guest'; // Default to 'Guest' if visitor_name is not provided
-    let clientIp = req.ip;
-    //remove conditional block in production
-    if(req.ip=="127.0.0.1"||"::1"){
-    clientIp="102.89.22.143"
-    console.log(clientIp)
-    }
-})
-app.listen(port, ()=>{
-    console.log("running on pprt",port);
-})
+app.get("/api/hello", async (req, res) => {
+  const visitorName = req.query.visitor_name || "Guest"; // Default to 'Guest' if visitor_name is not provided
+  let clientIp = await axios.get("https://ipapi.co/json/");
+
+  }
+);
+app.listen(port, () => {
+  console.log("running on pprt", port);
+});
